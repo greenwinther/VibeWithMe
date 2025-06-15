7b1d863c08e67e02dcaa95560755684e31b106f7
greenwinther <dennisgw@live.se>
2025-06-15 23:31:08 +0200

    refactor: simplify RoomLayout and RoomScreen components by removing unused variables and improving state management

--- app/rooms/[id]/_layout.tsx
+++ app/rooms/[id]/_layout.tsx
@@ -1,14 +1,12 @@
 import { ChatProvider } from "@/contexts/ChatContext";
 import { PlaylistProvider } from "@/contexts/PlaylistContext";
-import { Slot, useLocalSearchParams } from "expo-router";
+import { Slot } from "expo-router";
 import React from "react";
 
 export default function RoomLayout() {
-	const { id: roomId } = useLocalSearchParams<{ id: string }>();
-
 	return (
-		<PlaylistProvider roomId={roomId}>
-			<ChatProvider roomId={roomId}>
+		<PlaylistProvider>
+			<ChatProvider>
 				<Slot />
 			</ChatProvider>
 		</PlaylistProvider>
