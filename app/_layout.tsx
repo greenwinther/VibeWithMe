f057ea0bbadd94965fbeca0c5276ccb755c65739
greenwinther <dennisgw@live.se>
2025-06-18 14:50:54 +0200

    feat: enhance RootLayout component; integrate font loading with ActivityIndicator and improve layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,44 @@
-import { RoomProvider } from "@/contexts/RoomContext";
 import { UserProvider } from "@/contexts/UserContext";
+import { Colors } from "@/styles/theme";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import React from "react";
+import { ActivityIndicator, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+	const [fontsLoaded] = useFonts({
+		Shrikhand: require("../assets/fonts/Shrikhand-Regular.ttf"),
+		DMSerifDisplay: require("../assets/fonts/DMSerifDisplay-Regular.ttf"),
+		Inter: require("../assets/fonts/Inter-VariableFont_opsz,wght.ttf"),
+	});
+
+	if (!fontsLoaded) {
+		return (
+			<View
+				style={{
+					flex: 1,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: Colors.background,
+				}}
+			>
+				<ActivityIndicator />
+			</View>
+		);
+	}
+
 	return (
 		<SafeAreaProvider>
 			<UserProvider>
-				<RoomProvider>
-					<Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
-						{/* Lobby */}
-						<Stack.Screen name="index" />
-						{/* Dynamic Room pages */}
-						<Stack.Screen name="rooms/[id]" options={{ headerShown: true }} />
-						<Stack.Screen name="profile" options={{ title: "Profile", headerShown: true }} />
-					</Stack>
-				</RoomProvider>
+				<Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
+					{/* Lobby */}
+					<Stack.Screen name="index" />
+					{/* Dynamic Room pages */}
+					<Stack.Screen name="rooms/[id]" />
+					{/* User Profile page */}
+					<Stack.Screen name="profile" />
+				</Stack>
 			</UserProvider>
 		</SafeAreaProvider>
 	);
